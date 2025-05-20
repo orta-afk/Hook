a5f8a72fd3d54ccdfc5cd30fe7fa6af11e428331
orta-afk <darkkboy0013@gmail.com>
2025-05-20 16:56:51 +0530

    created main.c

--- src/main.c
+++ src/main.c
@@ -1,10 +1,20 @@
 #include "raylib.h"
 
-int main(){
-  InitWindow(600, 320, "lol");
-  while(!WindowShouldClose()){
+typedef struct {
+  const int width;
+  const int height;
+  const char *title;
+  Color color;
+} window;
+
+int main() {
+  window window = {
+      .width = 640, .height = 320, .title = "Hook", .color = BLACK};
+  InitWindow(window.width, window.height, window.title);
+  while (!WindowShouldClose()) {
     BeginDrawing();
-    ClearBackground(SKYBLUE);
+    ClearBackground(window.color);
     EndDrawing();
   }
+  return 0;
 }
