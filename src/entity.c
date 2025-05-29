eafc42269d367d27aec7563d30c1f0f32c5c2107
orta-afk <darkkboy0013@gmail.com>
2025-05-29 07:34:57 +0530

    now i am working like a pro {added .gitignore}

--- src/entity.c
+++ src/entity.c
@@ -1,8 +1,7 @@
 #include "entity.h"
 
 void initEntity(entity *entity) {
-  entity->positions =
-      (Vector2){GetScreenWidth() / 2, GetScreenHeight() / 2 - 50};
+  entity->positions = (Vector2){GetScreenWidth() / 2, GetScreenHeight() / 2 - 50};
   entity->velocity = (Vector2){0, 0};
   entity->gravity = 130;
   entity->max_gravity = 150;
